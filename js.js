201ade1790ec3a576fd33c74d0ea43b7fa4548a3
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-02-27 22:31:15 -0500

    add multiple operators

--- js.js
+++ js.js
@@ -1,19 +1,32 @@
 // Add an event listener to the calculator
-document.getElementById("my-calculator").addEventListener('submit', calculatorSubmit);
+document.getElementById("my-calculator").addEventListener('submit', calculatorSubmit)
 
 // Prevent the browser defeault, handle the calculator event
 function calculatorSubmit(event) {
     event.preventDefault();
-    var numOne = Number(document.getElementById("num-one").value);
-    var numTwo = Number(document.getElementById("num-two").value);
-    var result = document.getElementById("result");
+    var numOne = Number(document.getElementById("num-one").value)
+    var numTwo = Number(document.getElementById("num-two").value)
+    var numOperator = document.getElementById("num-operator").value
+    var result = document.getElementById("result")
     
-    var calculation = calculate(numOne, numTwo);
-   
-    result.innerHTML = calculation;
+    var calculation = calculate(numOne, numTwo, numOperator)
+
+    result.innerHTML = calculation
 }
 
-//Add two numbers together... for now (-,*,% coming soon)
-function calculate(n1, n2){
-    return n1 + n2;
+//Calculate two numbers
+function calculate(n1, n2, operator){
+
+   switch(operator){
+       case 'add':
+           return n1 + n2
+       case 'subtract':
+           return n1 - n2
+       case 'multiply':
+           return n1 * n2
+       case 'devide':
+           return n1 / n2
+        default:
+            return "How did you do that?"
+   }
 } 
